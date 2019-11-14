911797b0b6fa13f2cf8b4c1023f24f39bcf53c0d
david <djgtorrens@gmail.com>
2019-11-14 11:42:16 +0000

    Sorting out .gitignore

--- tmux_start.sh
+++ tmux_start.sh
@@ -2,11 +2,12 @@
 cd /home/pi/code
 #tmux kill-session -t wd
 #tmux kill-session -t ctrl
-#tmux kill-session -t test
+tmux kill-session -t test
 #tmux kill-session -t mnt
+tmux kill-session -t cpu
 #tmux new-session -d -s wd 'python3 watch_dog.py'
 #tmux new-session -d -s ctrl 'python3 temp_control.py'
-#tmux new-session -d -s cpu 'python3 cpu_monitor.py'
+tmux new-session -d -s cpu 'python3 cpu_monitor.py'
 #tmux new-session -d -s mnt 'python3 mount_drives.py'
-#tmux new-session -d -s test 'python3 test_text_buffer.py'
+tmux new-session -d -s test 'python3 test_text_buffer.py'
 

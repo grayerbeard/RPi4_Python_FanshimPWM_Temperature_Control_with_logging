3a0d12b25bd1470d0bee3f28119999fd485094b5
david <djgtorrens@gmail.com>
2019-11-15 23:55:16 +0000

    removing empty lines tmux_stress

--- tmux_stress.sh
+++ tmux_stress.sh
@@ -2,5 +2,3 @@
 cd /home/pi/code
 tmux kill-session -t stress
 tmux new-session -d -s stress 'while true; do vcgencmd measure_clock arm; vcgencmd measure_temp; sleep 10; done& stress -c 4 -t 900s'
-
-							

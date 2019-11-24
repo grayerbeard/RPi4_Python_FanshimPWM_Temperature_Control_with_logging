eccc511d96ec56a5070992c98437b05469dfb631
David Torrens <djgtorrens@gmail.com>
2019-11-24 21:31:49 +0000

    edited so only stops fanshim

--- tmux_stop.sh
+++ tmux_stop.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /home/pi/code
-tmux kill-session -t ctrl
-tmux kill-session -t cpu
-tmux kill-session -t test
-tmux kill-session -t wd
-tmux kill-session -t mnt
+tmux kill-session -t fanshim
 

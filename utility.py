b6f3eb54514ca6bc4ccf33da719cf32e403a0415
David Torrens <djgtorrens@gmail.com>
2019-11-24 23:01:44 +0000

    preamble updated

--- utility.py
+++ utility.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#   for use with Python 3
-
-#	utility.py
-#  
-#	This program is free software; you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation; either version 2 of the License, or
-#	(at your option) any later version.
+# This file is part of pwm_fanshim.
+# Copyright (C) 2015 Ivmech Mechatronics Ltd. <bilgi@ivmech.com>
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNimport sys, getoptESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#  
-#	You should have received a copy of the GNU General Public License
-#	along with this program; if not, write to the Free Software
-#	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#	MA 02110-1301, USA.
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# title           :utility.py
+# description     :pwm control for R Pi Cooling Fan
+# author          :David Torrens
+# start date      :2019 11 20
+# version         :0.1
+# python_version  :3
 
 # Standard library imports
 from csv import DictReader as csv_DictReader

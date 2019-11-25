b7147fdb3d6c4edde0892c9f3204693932343b5c
David Torrens <djgtorrens@gmail.com>
2019-11-25 11:03:06 +0000

    going to code not fanshim
    probably makes no difference unless code direcory not there

--- tmux_stress.sh
+++ tmux_stress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/pi/code
+cd /home/pi/fanshim
 tmux kill-session -t stress
 tmux new-session -d -s stress 'while true; do vcgencmd measure_clock arm; vcgencmd measure_temp; sleep 10; done& stress -c 4 -t 900s'
